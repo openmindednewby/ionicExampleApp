150381394b1f8445919f5aa81db92f14fcbb5748
Demetrios Loizides <loizidesdemetris@gmail.com>
2024-12-22 13:14:07 +0200

    minor change

--- Angular/src/IonicExampleApp/src/app/tabs/tabs.routes.ts
+++ Angular/src/IonicExampleApp/src/app/tabs/tabs.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
       {
         path: 'tab1',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('../cameraRecordingTab/cameraRecordingTab.page').then((m) => m.Tab1Page),
       },
       {
         path: 'tab2',
