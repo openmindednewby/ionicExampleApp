e011052c58ca54d0f175a15c14cd6c542256b5f4
Demetrios Loizides <loizidesdemetris@gmail.com>
2024-12-24 23:06:39 +0200

    improved styles a bit

--- Angular/src/IonicExampleApp/src/app/views/search-pictures-tab/search-pictures-tab.ts
+++ Angular/src/IonicExampleApp/src/app/views/search-pictures-tab/search-pictures-tab.ts
@@ -1,5 +1,5 @@
 import { Component, computed, signal } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFab, IonIcon, IonImg } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFab, IonIcon, IonCard } from '@ionic/angular/standalone';
 import { IndexedDbService, ObjectStoreNames } from 'src/app/services/indexed-db/indexed-db.service';
 import { CommonModule } from '@angular/common';
 import { isValueDefined } from 'src/app/utils/tools/isValueDefined';
@@ -8,7 +8,7 @@ import { isValueDefined } from 'src/app/utils/tools/isValueDefined';
   selector: 'appSearchPicturesTab',
   templateUrl: 'search-pictures-tab.html',
   styleUrls: ['search-pictures-tab.scss'],
-  imports: [CommonModule, IonButton, IonFab, IonImg, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCard, CommonModule, IonButton, IonFab, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class SearchPicturesTab {
   private currentPhoto = signal<Blob | undefined>(undefined);
@@ -17,10 +17,18 @@ export class SearchPicturesTab {
     if (isValueDefined(photo)) return URL.createObjectURL(photo!);
     return;
   });
+  public showCard = computed(() => isValueDefined(this.photoURL()));
 
   constructor(
     private indexedDbService: IndexedDbService,
   ) { }
+
+  public downloadPicture(): void {
+    const link = document.createElement('a');
+    link.href = this.photoURL()!;
+    link.download = 'picture.jpg';
+    link.click();
+  }
 
   public async getImage(id: number): Promise<void> {
     const photo = await this.indexedDbService.getItem<Blob>(id, ObjectStoreNames.Pictures);
