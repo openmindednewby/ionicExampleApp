9021e26b118cbc627ace870a27d0c1edd55f1235
Demetrios Loizides <loizidesdemetris@gmail.com>
2024-12-28 16:13:29 +0200

    checkpoint need to fix this

--- Angular/src/IonicExampleApp/src/app/services/http/getRandomPicture/get-random-picture.service.ts
+++ Angular/src/IonicExampleApp/src/app/services/http/getRandomPicture/get-random-picture.service.ts
@@ -2,23 +2,25 @@ import { Injectable, signal, Signal } from '@angular/core';
 import { HttpService } from '../http.service';
 import { Endpoints } from 'src/environments/endpoints';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GetRandomPictureService { 
+export class GetRandomPictureService {
+  private pictureSubject = new BehaviorSubject<Blob | undefined>(undefined);
+  public picture$ = this.pictureSubject.asObservable();
 
   constructor(private httpService: HttpService) { }
 
-  public getPicture(width: number = 500, height: number = 500): Signal<Blob | undefined> {
+  public getPicture(width: number = 500, height: number = 500): void {
     try {
       const urlIDs = [width.toString(), height.toString()];
-      const picture$ = this.httpService.httpGet<undefined, Blob>(Endpoints.GetPicsumRandomPhoto, urlIDs)
+      this.httpService.httpGet<undefined, Blob>(Endpoints.GetPicsumRandomPhoto, urlIDs).pipe(
+        map(response => this.pictureSubject.next(response)));
 
-      return toSignal<Blob>(picture$);
     } catch (error) {
       console.log('Failed to get random image: ' + error);
-      return signal<Blob | undefined>(undefined);
     }
   }
 }
--- Angular/src/IonicExampleApp/src/app/views/tabs/components/take-picture-tab/take-picture-tab.ts
+++ Angular/src/IonicExampleApp/src/app/views/tabs/components/take-picture-tab/take-picture-tab.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, computed, Signal, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, computed, OnDestroy, Signal, signal } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonIcon, IonButton, IonImg } from '@ionic/angular/standalone';
 import { CameraServiceService } from '../../../../services/native/camera-service/camera-service.service';
 import { CommonModule } from '@angular/common';
@@ -8,6 +9,7 @@ import { isValueDefined } from 'src/app/utils/tools/isValueDefined';
 import dataUrlToBlob from 'src/app/utils/mappers/dataUrlToBlob';
 import { Header } from "../header/header";
 import { GetRandomPictureService } from 'src/app/services/http/getRandomPicture/get-random-picture.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'appTakePictureTab',
@@ -17,23 +19,30 @@ import { GetRandomPictureService } from 'src/app/services/http/getRandomPicture/
   imports: [CommonModule, IonImg, IonButton, IonFab, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, Header],
 })
 export class TakePictureTab {
-  public currentPicture = signal<Photo | undefined>(undefined);
-  public isStorePictureDisabled = computed(() => !isValueDefined(this.currentPicture()?.dataUrl));
+  public randomPicture: Signal<Photo | undefined>;
+  public takenPicture = signal<Photo | undefined>(undefined);
+  public currentPicture = computed(() => {
+    return this.takenPicture() || this.randomPicture();
+  });
+  public isStorePictureDisabled = computed(() => !isValueDefined(this.takenPicture()?.dataUrl));
 
   constructor(
     private cameraServiceService: CameraServiceService,
     private indexedDbService: IndexedDbService,
     private getRandomPictureService: GetRandomPictureService
-  ) { }
+  ) {
+      const picture$ = this.getRandomPictureService.picture$;
+      const picture = toSignal(picture$);//manges subscription and un-subscription
+      this.randomPicture = computed(() => {
+        if (isValueDefined(picture())) return { dataUrl: URL.createObjectURL(picture()!) } as Photo;
+        return;
+      });
+   }
 
   public getRandomPicture(): void {
     try {
-      const pictureSignal = this.getRandomPictureService.getPicture();
-      const picture = pictureSignal();
-      console.log('picture', picture);
-      if(!isValueDefined(picture)) throw new Error('Failed to get random image');
+      this.getRandomPictureService.getPicture();
 
-      this.currentPicture.set({ dataUrl: URL.createObjectURL(picture!) } as Photo);
     } catch (error) {
       console.error('Error taking picture:', error);
     }
@@ -42,17 +51,16 @@ export class TakePictureTab {
   public async takePicture(): Promise<void> {
     try {
       const photo = await this.cameraServiceService.takePhoto();
-      this.currentPicture.set(photo);
+      this.takenPicture.set(photo);
     } catch (error) {
       console.error('Error taking picture:', error);
     }
   }
 
-  // Store the current photo in IndexedDB
   public async storePicture(): Promise<void> {
-    if (isValueDefined(this.currentPicture()!.dataUrl)) {
+    if (isValueDefined(this.takenPicture()!.dataUrl)) {
       try {
-        const blob = dataUrlToBlob(this.currentPicture()!.dataUrl!);
+        const blob = dataUrlToBlob(this.takenPicture()!.dataUrl!);
         await this.indexedDbService.addItem(blob, ObjectStoreNames.Pictures);
       } catch (error) {
         console.error('Error storing photo:', error);
